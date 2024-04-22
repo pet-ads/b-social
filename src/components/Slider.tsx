85791c757a2039cf2add0af5e45c37f788aaf587
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-22 14:35:01 -0300

    feat: implement item-by-item scrolling control

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,8 @@
+import "./Slider.css";
 import { PanInfo, motion } from "framer-motion";
-import imgDev1 from "../public/img/imgDev1.jpg";
-import imgDev2 from "../public/img/imgDev2.jpg";
-import imgDev3 from "../public/img/imgDev3.jpg";
+import imgDev1 from "../../public/Images/imgDev1.jpg";
+import imgDev2 from "../../public/Images/imgDev2.jpg";
+import imgDev3 from "../../public/Images/imgDev3.jpg";
 import { useState, useRef, useEffect } from "react";
 
 const images = [imgDev1, imgDev2, imgDev3];
@@ -26,7 +27,7 @@ export default function Slider() {
   const handleDragEnd = (_: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) => {
     const offset = info.offset.x;
     const direction = offset > 0 ? -1 : 1;
-    const threshold = 50; // Define how sensitive the drag should be
+    const threshold = 50;
     const newIndex = activeIndex + (Math.abs(offset) > threshold ? direction : 0);
     if (newIndex >= 0 && newIndex < images.length) {
       setActiveIndex(newIndex);
@@ -36,7 +37,7 @@ export default function Slider() {
 
   return (
     <motion.div className="slider">
-      <motion.div>
+      <motion.div className="carousel" ref={carousel} whileTap={{ cursor: "grabbing" }}>
         <motion.div
           className="inner"
           drag="x"
