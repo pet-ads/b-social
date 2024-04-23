3d776d10ce63306b68c97c89e821c35cce2ca62f
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-23 16:55:49 -0300

    feat: implementado card em App.tsx com novos atributos de

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import "./App.css";
 import { motion } from "framer-motion";
 import Slider from "./components/Slider/Slider";
 import images from "../public/data/data.json";
+import Card from "./components/Card/Card";
+import { TagType } from "./components/Card/subComponent/TagTypeInterface";
 
 function App() {
+  const tag: TagType = {text: "etaa", backgroundColor: "blue", textColor: "white", number: "59"};
+  
   return (
     <>
       <div className="App">
@@ -16,6 +20,9 @@ function App() {
           ))}
         </Slider>
       </div>
+      <Card image={"exImage_1.png"} title={"Titulo"} text={
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus faucibus efficitur velit, a pharetra ligula venenatis at. Vestibulum luctus ante et purus iaculis, nec consequat nibh faucibus.'}
+        tag={tag}/>
     </>
   );
 }
