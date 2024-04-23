6558d718a625ce17f28d0e4785fa2abedf11135a
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-23 16:03:42 -0300

    fix: reestruturando Tap para receber numero de card de referencia e personalizar cores de letras e background

--- src/components/Card/subComponent/Tag.tsx
+++ src/components/Card/subComponent/Tag.tsx
@@ -1,10 +1,13 @@
 import {TagType} from "./TagTypeInterface"
 import "./TagStyle.css"
 
-export default function Tag({text, color}: TagType) {
+export default function Tag({text, textColor, backgroundColor, number}: TagType) {
     return (
-        <div className="TagContainer" style={{backgroundColor: color}}>
-            <p>{text}</p>
+        <div className="TagContainer">
+            <div className="Tag" style={{backgroundColor: backgroundColor, color: textColor}}>
+                <p>{text}</p>
+            </div>
+            <p>{number}</p>
         </div>
         
     );
