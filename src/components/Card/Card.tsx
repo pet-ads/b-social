6739f295f95d41a91d11a0ecc1fe5e8e98394f6a
Eduardo Derisso <dudu.derisso@gmail.com>
2024-04-23 16:07:12 -0300

    fix: Card modificado para mudar cor de letras do conteudo, e melhor estrutura parte do Tap

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,17 +10,21 @@ interface CardType {
 }
 
 export default function Card({image, title, text, tag}: CardType) {
+    const imagePath: string = "./../../../public/Images/" + image;
+
     return (
         <div className="cardContainer">
             <div className="cardImage">
-                {image}
+                <img className="cardImage" src={imagePath} />
             </div>
-            <div className="cardContent">
-                <div className="cardTitle">{title}</div>
-                <div className="cardText">{text}</div>
+            <div className="cardContent" style={{color: "#603A17"}}>
+                <div className="cardMainContent">
+                    <div className="cardTitle">{title}</div>
+                    <div className="cardText">{text}</div>
+                </div>
                 <div className="indicatedTopicContainer">
-                    <div className="indicatedTopicTitle"><p>Veja também</p></div>
-                    <Tag text={tag.text} color={tag.color}/>
+                    <p className="indicatedTopicTitle">Veja também</p>
+                    <Tag text={tag.text} textColor={tag.textColor} backgroundColor={tag.backgroundColor} number={tag.number}/>    
                 </div>
             </div>
         </div>
