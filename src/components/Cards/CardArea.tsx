31eb8651e8decb58150475c10fedcbb60f96c4de
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-12 01:16:38 -0300

    feat: Add Card  component

--- src/components/Cards/CardArea.tsx
+++ src/components/Cards/CardArea.tsx
@@ -1,26 +1,18 @@
 import "./cardArea.css";
-import walkingBear from "../../../public/Images/walkingBear.gif";
+import Card from "./subcomponents/Card";
 export default function CardArea() {
   return (
     <div className="cardArea">
       <div className="CardRow">
-        <div className="Card">
-          <a className="anchor">
-            <div className="imgArea">
-              <div className="imgConteiner">
-                <img className="CardImg" decoding="async" src={walkingBear} />
-              </div>
-            </div>
-          </a>
-          <div className="textArea">
-            <div className="cardTitle">
-              <h3 className="h3">Amor</h3>
-            </div>
-            <div className="cardText">
-              <p className="info">Learn about hosting built for scale and reliability.</p>
-            </div>
-          </div>
-        </div>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   );
