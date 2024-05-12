6936b71dac1c81ac239debb7057aea75d6f908dc
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-11 23:07:33 -0300

    feat: Add navigation style structure

---
+++ src/components/nav/navigation.tsx
@@ -0,0 +1,24 @@
+export default function Navigation() {
+  return (
+    <div className="NavConteiner">
+      <nav className="navigation">
+        <div className="textConteiner">Beaba</div>
+        <div className="navigationButtons">
+          <div className="link">
+            <p>Quem Somos</p>
+          </div>{" "}
+          <div className="link">
+            <p>Quem Somos</p>
+          </div>
+          <div className="link">
+            <p>Quem Somos</p>
+          </div>
+          <div className="link">
+            <p>Quem Somos</p>
+          </div>
+        </div>
+        <div className="divider"></div>
+      </nav>
+    </div>
+  );
+}
