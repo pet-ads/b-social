c2212164f40f8283bc22552be3af17f52c116efa
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-13 14:39:47 -0300

    feat: Implement defaultLinks component and responsive render logic

--- src/components/nav/navigation.tsx
+++ src/components/nav/navigation.tsx
@@ -1,24 +1,27 @@
 import "./nav.css";
+import DefaultLinks from "./subcomponents/defaultLinks";
+import { useEffect, useState } from "react";
 
 export default function Navigation() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="NavConteiner">
       <nav className="navigation">
         <div className="textConteiner">Beaba</div>
-        <div className="navigationButtons">
-          <div className="link">
-            <p>Guia</p>
-          </div>{" "}
-          <div className="link">
-            <p>Quem Somos</p>
-          </div>
-          <div className="link">
-            <p>Contato</p>
-          </div>
-          <div className="link">
-            <p>Como ajudar</p>
-          </div>
-        </div>
+        <div className="navigationButtons">{windowWidth <= 809 ? "fazendo ainda" : <DefaultLinks />}</div>
         <div className="divider"></div>
       </nav>
     </div>
