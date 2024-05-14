a303fe852272936ba8c4fb45d9e9364c2e8d9432
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-14 14:06:40 -0300

    feat: Add isSelectedLogic

--- src/components/Language/language.tsx
+++ src/components/Language/language.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import "./langSetting.css";
 
 interface ILangSettings {
   setSelectedPath: React.Dispatch<React.SetStateAction<string>>;
 }
+
 export default function LangSettings({ setSelectedPath }: ILangSettings) {
+  const [selectedLang, setSelectedLang] = useState<string>("pt-br");
+
+  const handleClick = (lang: string) => {
+    setSelectedLang(lang);
+    setSelectedPath(lang);
+  };
+
   return (
     <div className="conteiner">
       <div className="logoBtnConteiner">
-        <div className="logo" onClick={() => setSelectedPath("pt-br")}>
+        <div className={`logo ${selectedLang === "pt-br" ? "selected" : ""}`} onClick={() => handleClick("pt-br")}>
           IconBr
         </div>
-        <div className="logo" onClick={() => setSelectedPath("en")}>
+        <div className={`logo ${selectedLang === "en" ? "selected" : ""}`} onClick={() => handleClick("en")}>
           IconUSA
         </div>
       </div>
