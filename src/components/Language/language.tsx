30c57d1a52ba373caa0e150945a9f279096710df
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-14 13:49:39 -0300

    feat: add langSettings.css file

--- src/components/Language/language.tsx
+++ src/components/Language/language.tsx
@@ -1,27 +1,18 @@
+import "./langSetting.css";
+
 interface ILangSettings {
   setSelectedPath: React.Dispatch<React.SetStateAction<string>>;
 }
 export default function LangSettings({ setSelectedPath }: ILangSettings) {
   return (
-    <div
-      style={{
-        width: "88%",
-        marginTop: "20px",
-        display: "flex",
-        justifyItems: "center",
-      }}
-    >
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "flex-end",
-          gap: "10px",
-        }}
-      >
-        <div onClick={() => setSelectedPath("pt-br")}>IconBr</div>
-        <div onClick={() => setSelectedPath("en")}>IconUSA</div>
+    <div className="conteiner">
+      <div className="logoBtnConteiner">
+        <div className="logo" onClick={() => setSelectedPath("pt-br")}>
+          IconBr
+        </div>
+        <div className="logo" onClick={() => setSelectedPath("en")}>
+          IconUSA
+        </div>
       </div>
     </div>
   );
