c2cf4459670c4d802d00c4fa4c2b75483abc1c47
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-15 12:23:23 -0300

    fix: Fix merge issues

--- src/components/Language/language.tsx
+++ src/components/Language/language.tsx
@@ -9,15 +9,7 @@ interface ILangSettings {
 
 export default function LangSettings({ setSelectedPath, selectedPath }: ILangSettings) {
   return (
-    <div className="conteiner-lang">
-      <div className="logoBtnConteiner">
-        <div className={`logo ${selectedPath === "pt-br" ? "selected" : ""}`} onClick={() => setSelectedPath("pt-br")}>
-          <img className="logopic" src={Brlogo} alt="logo bandeira do Brasil" />
-        </div>
-        <div className={`logo ${selectedPath === "en" ? "selected" : ""}`} onClick={() => setSelectedPath("en")}>
-          <img className="logopic" src={EUALogo} alt="logo bandeira do Brasil" />
-        </div>
-    <div className="conteiner-lang">
+    <div className="continer-lang">
       <div className="logoBtnContainer">
         {selectedPath === "en" ? (
           <div className="logo selected" onClick={() => setSelectedPath("pt-br")}>
