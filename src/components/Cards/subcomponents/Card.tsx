662238976829216e17f482f1117e09d1fbbd63fb
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-05-16 14:24:55 -0300

    feat: Adding modal component to cards to enable modal opening

--- src/components/Cards/subcomponents/Card.tsx
+++ src/components/Cards/subcomponents/Card.tsx
@@ -5,11 +5,12 @@ interface CardContentI {
   CardTitle: string;
   CardText: string;
   bgColor: string;
+  onClick?: () => void;
 }
-export default function Card({ CardImg, CardTitle, CardText, bgColor }: CardContentI) {
+export default function Card({ CardImg, CardTitle, CardText, bgColor, onClick}: CardContentI) {
   return (
     <div className="Card">
-      <a className="anchor" style={{ backgroundColor: `${bgColor}` }}>
+      <a className="anchor" style={{ backgroundColor: `${bgColor}`, cursor: 'pointer'}} onClick={onClick}>
         <div className="imgArea">
           <div className="imgConteiner">
             <img className="CardImg" decoding="async" src={CardImg} />
