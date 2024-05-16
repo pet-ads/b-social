e89c311878cd833cb474a23c10977daa026c398b
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-05-16 14:29:10 -0300

    feat: Implement interactive modals for card selection

--- src/components/Cards/CardArea.tsx
+++ src/components/Cards/CardArea.tsx
@@ -1,5 +1,7 @@
 import "./cardArea.css";
 import Card from "./subcomponents/Card";
+import { useState } from 'react';
+import Modal from "../Modal/Modal";
 
 interface CardContentI {
   id: string;
@@ -14,6 +16,12 @@ interface iData {
 }
 
 export default function CardArea({ data }: iData) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState<CardContentI | null>(null);
+  const openModal = (card: CardContentI) => {
+    setSelectedCard(card);
+    setIsModalOpen(true);
+  };
   return (
     <div className="cardArea">
       <div className="CardRow">
@@ -24,9 +32,19 @@ export default function CardArea({ data }: iData) {
             CardTitle={item.CardTitle}
             CardText={item.CardText}
             bgColor={item.bgColor}
+            onClick={() => openModal(item)}
           />
         ))}
       </div>
+      {isModalOpen && selectedCard && (
+        <Modal
+          CardImg={selectedCard.CardImg}
+          CardTitle={selectedCard.CardTitle}
+          CardText={selectedCard.CardText}
+          bgColor={selectedCard.bgColor}
+          closeModal={() => setIsModalOpen(false)}
+        />
+      )}
     </div>
   );
 }
