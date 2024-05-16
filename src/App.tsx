2fe63d28e170ba73767cd94fe62c1a3808961801
Eduardo Derisso <dudu.derisso@gmail.com>
2024-05-16 11:12:36 -0300

    fixing: add props necessaria ao componente no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Search from "./components/search/Search";
 import CardArea from "./components/Cards/CardArea";
 import HelpArea from "./components/HelpArea/HelpArea";
 import Navigation from "./components/nav/navigation";
+import LangSettings from "./components/Language/language";
 
 function App() {
   const URL_DATA_PTBR = "/data/dataPTBR.json";
