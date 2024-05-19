c6cae0f5dc7736ae72bd5f2086a69f85d0fdc060
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-05-19 13:35:27 -0300

    feat: implement switch flag logic

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -1,26 +1,51 @@
+import { useState } from "react";
 import "./dropdown.css";
+import languagesJson from "../../../public/data/languages.json";
+
+interface Language {
+  pathToLogo: string;
+  pathToData: string;
+}
+
+const languages: Language[] = languagesJson.languages;
 
 export default function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState<Language | null>(null);
+
+  const handleLanguageClick = (lang: Language) => {
+    setSelectedLanguage(lang);
+    setIsOpen(false); // Fechar o dropdown ao selecionar uma bandeira
+  };
+
   return (
     <nav>
       <ul>
-        <li>
-          <a href="">Home</a>
-        </li>
-        <li>
-          <a href="">Sobre</a>
-        </li>
-        <li className="dropdown">
-          <a href="">Serviços</a>
+        <li
+          className="dropdown"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {selectedLanguage ? (
+            <img className={"flag"} src={selectedLanguage.pathToLogo} alt="Selected flag" />
+          ) : (
+            "Selecionar idioma"
+          )}
 
-          <div className="dropdown-menu">
-            <a href="">Serviço 1</a>
-            <a href="">Serviço 2</a>
-            <a href="">Serviço 3</a>
+          <div className="dropdown-menu" style={{ display: isOpen ? "block" : "none" }}>
+            <div className="flag-container">
+              {languages.map((lang, index) => (
+                <img
+                  className="flag"
+                  key={index}
+                  src={lang.pathToLogo}
+                  alt={`Logo ${index}`}
+                  onClick={() => handleLanguageClick(lang)}
+                />
+              ))}
+            </div>
           </div>
-        </li>
-        <li>
-          <a href="">Contato</a>
         </li>
       </ul>
     </nav>
